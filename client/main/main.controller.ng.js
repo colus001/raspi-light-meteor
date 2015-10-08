ed9a9895ca6e7a0df174b2e3c4d29062dd96eed7
Seokjun Kim <colus001@me.com>
2015-10-08 23:24:47 +0900

    Fix typo and update add color util

--- client/main/main.controller.ng.js
+++ client/main/main.controller.ng.js
@@ -10,4 +10,10 @@ angular.module('raspilightMeteorApp')
   $scope.setColor = function (colors) {
     $meteor.call('setColors', colors);
   }
+
+  $scope.getRgba = function (color) {
+    color = _.extend({ red: 0, green: 0, blue: 0, alpha: 100 }, color);
+    return `rgba(${color.red}, ${color.green}, ${color.blue}, ${color.alpha/100})`;
+    // getRgba(color)
+  }
 });
--- server/hardware/light.js
+++ server/hardware/light.js
@@ -1,6 +1,6 @@
 var ws281x = Meteor.npmRequire('rpi-ws281x-native');
 var NUM_LEDS = 16;
-var neopixel = null;
+var neoPixel = null;
 
 class Light {
   constructor (options) {
@@ -13,7 +13,7 @@ class Light {
     var options = _.extend([0,0,0], [ colors.red, colors.green, colors.blue ]);
 
     for ( var i = 0; i < this.numberOfLeds; i++) {
-      this.pixelData[i] = this.rgb2Int(options[0], options[1], options[2]);
+      this.pixelData[i] = NeoPixelUtil.rgb2Int(options[0], options[1], options[2]);
     }
   }
 
@@ -28,19 +28,12 @@ class Light {
 
     this.lightInterval = setInterval(function () {
       for (var i = 0; i < NUM_LEDS; i++) {
-        pixelData[i] = this.colorwheel((offset + i) % 256);
+        pixelData[i] = NeoPixelUtil.colorwheel((offset + i) % 256);
       }
 
       offset = (offset + 1) % 256;
       ws281x.render(pixelData);
     }, 1000 / 30);
-  }
-
-  colorwheel(pos) {
-    pos = 255 - pos;
-    if (pos < 85) { return this.rgb2Int(255 - pos * 3, 0, pos * 3); }
-    else if (pos < 170) { pos -= 85; return this.rgb2Int(0, pos * 3, 255 - pos * 3); }
-    else { pos -= 170; return this.rgb2Int(pos * 3, 255 - pos * 3, 0); }
   }
 
   light () {
@@ -52,11 +45,17 @@ class Light {
     this.setColors(0,0,0);
     ws281x.render(this.pixelData);
     clearInterval(this.lightInterval);
-
   }
+}
 
+NeoPixelUtil = {
+  colorwheel(pos) {
+    pos = 255 - pos;
+    if (pos < 85) { return this.rgb2Int(255 - pos * 3, 0, pos * 3); }
+    else if (pos < 170) { pos -= 85; return this.rgb2Int(0, pos * 3, 255 - pos * 3); }
+    else { pos -= 170; return this.rgb2Int(pos * 3, 255 - pos * 3, 0); }
+  },
   rgb2Int(r, g, b) {
-    console.log(r,g,b);
     return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
   }
 }
@@ -64,7 +63,7 @@ class Light {
 Meteor.startup(function () {
   try {
     ws281x.init(NUM_LEDS);
-    neopixel = new Light({
+    neoPixel = new Light({
       numberOfLeds: NUM_LEDS
     });
     console.log("Initialization completed.");
@@ -80,15 +79,15 @@ Meteor.methods({
   light (options) {
     switch (options.mode) {
       case 'on':
-        neopixel.light();
+        neoPixel.light();
         break;
       case 'off':
         console.log('Light Off');
-        neopixel.turnoff();
+        neoPixel.turnoff();
         break;
       case 'rainbow':
         console.log('Rainbow');
-        neopixel.rainbow();
+        neoPixel.rainbow();
         break;
       default:
         console.log('MATCH_NOT_FOUND');
@@ -97,6 +96,6 @@ Meteor.methods({
   },
 
   setColors (colors) {
-    neopixel.setColors(colors);
+    neoPixel.setColors(colors);
   }
 });
